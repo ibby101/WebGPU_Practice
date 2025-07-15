47947b357c2478cc5320aa1ff1c49507b1d1cc93
Ibrahim Abdi <ibroabdi@outlook.com>
2025-07-15 22:46:20 +0100

    mesh upload function completed
    full usage of the mesh upload feature not integrated yet, need to adjust the WGSL (shader language) to accommodate for uploading meshes, and add a button for this as well.

--- src/buttons/mesh_upload.ts
+++ src/buttons/mesh_upload.ts
@@ -17,3 +17,107 @@ type CacheArray<T> = T[][]
 type toBeFloat = number
 
 type toBeUInt16 = number
+
+export default class Objloader{
+
+    constructor() {}
+
+    // this function loads the OBJ file from the given file path
+    // and returns the file content as a string
+
+    async load(filePath: FilePath): Promise<ObjFile> {
+        const response = await fetch(filePath);
+        if (!response.ok) {
+            throw new Error(`Failed to load OBJ file: ${response.statusText}`);
+        }
+
+        const file = await response.text();
+
+        if (file.length === 0) {
+            throw new Error("OBJ file is empty.");
+        }
+        
+        return file;
+    }
+
+
+    // Parses the OBJ file content and returns a Mesh object
+
+    parse(file: ObjFile): Mesh {
+        const lines = file?.split("\n")
+
+        // storing the data in cache arrays, to avoid creating new arrays every time
+
+        const cachedPositions: CacheArray<CachePosition> = []
+        const cachedFaces: CacheArray<CacheFace> = []
+        const cachedNormals: CacheArray<CacheNormal> = []
+        const cachedUVs: CacheArray<CacheUV> = []
+
+        
+        // reading the file line by line, and storing the data in the cache arrays
+
+        {
+            for (const untrimmedLine of lines) {
+                const line = untrimmedLine.trim(); // removes whitespace from both ends
+                const [startingChar, ...data] = line.split(" ");
+                switch (startingChar) {
+                    case 'v': // vertex position
+                        cachedPositions.push(data.map(parseFloat))
+                        break
+                    case 'vt': // texture coordinate
+                        cachedUVs.push(data.map(parseFloat))
+                        break
+                    case 'vn': // vertex normal
+                        cachedNormals.push(data.map(parseFloat))
+                        break
+                    case 'f': // face
+                        cachedFaces.push(data.map(parseFloat))
+                        break
+                }
+            }
+        }
+
+        // converting the cache arrays to the final arrays, and removing duplicates
+
+        const finalPositions: toBeFloat[] = []
+        const finalUVs: toBeFloat[] = []
+        const finalNormals: toBeFloat[] = []
+        const finalIndices: toBeUInt16[] = []  
+
+        {
+            const cache: Record<string, number> = {}
+            let i = 0
+            
+            for (const faces of cachedFaces){
+                for (const faceString of faces) {
+                    if (cache[faceString]) {
+                        finalIndices.push(cache[faceString])
+                        continue
+                }
+
+                cache[faceString] = i
+                finalIndices.push(i)
+
+
+                const [vertexIndex, uvIndex, normalIndex] = faceString.toString().split('/').map(Number);
+
+                
+                // using the indices to get the positions, UVs, and normals from the cache arrays
+
+                vertexIndex > -1 && finalPositions.push(...cachedPositions[vertexIndex]);
+                uvIndex > -1 && finalUVs.push(...cachedUVs[uvIndex]);
+                normalIndex > -1 && finalNormals.push(...cachedNormals[normalIndex]);
+
+                i++
+                }
+            }
+        }
+
+        return {
+            positions: new Float32Array(finalPositions),
+            uvs: new Float32Array(finalUVs),
+            normals: new Float32Array(finalNormals),
+            indices: new Uint16Array(finalIndices),
+        }
+    }
+}
